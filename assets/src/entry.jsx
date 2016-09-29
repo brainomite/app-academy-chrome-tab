76db801ef2e67237368991eaa21a7c36aa987d63
Tyler Torola <tyler@tjt.codes>
2016-09-29 11:55:52 -0700

    refactors entry.js stamp

--- assets/src/entry.jsx
+++ assets/src/entry.jsx
@@ -5,27 +5,25 @@ import App from 'components/app';
 import Links from 'links';
 
 document.addEventListener("DOMContentLoaded", () => {
-	var Stamp = (function(){
-		var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
-								'Thursday', 'Friday', 'Saturday'];
+	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+							'Thursday', 'Friday', 'Saturday'];
 
-		var months = ['January', 'February', 'March', 'April',
-									'May', 'June', 'July', 'August', 'September',
-									'October', 'November', 'December'];
+	const months = ['January', 'February', 'March', 'April',
+								'May', 'June', 'July', 'August', 'September',
+								'October', 'November', 'December'];
 
-		var dateStamp;
-		var d = new Date();
-		var hours = d.getHours();
+	const d = new Date();
+	const hours = d.getHours();
 
-		dateStamp = days[d.getDay()].substring(0,3) + ", ";
-		dateStamp += months[d.getMonth()].substring(0,3) + " ";
-		dateStamp += d.getDate();
+	let dateStamp;
+	dateStamp = days[d.getDay()].substring(0,3) + ", ";
+	dateStamp += months[d.getMonth()].substring(0,3) + " ";
+	dateStamp += d.getDate();
 
-		return {
-			date: dateStamp,
-			time: dateStamp + ", " + hours
-		};
-	})();
+	const Stamp = {
+		date: dateStamp,
+		time: dateStamp + ", " + hours
+	};
 
 	ReactDom.render(<App stamp={Stamp} links={Links} />, document.querySelector('#app'));
 });
