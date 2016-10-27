952428ece9392c507e73856d7b1931509f36569f
Tyler Torola <tyler@tjt.codes>
2016-10-27 11:58:19 -0700

    reorders links src for clarity

--- assets/src/components/page/links/view.jsx
+++ assets/src/components/page/links/view.jsx
@@ -2,22 +2,10 @@ import React from 'react';
 
 import { MAIN_LINKS } from 'util/links';
 
-const linksListItem = (link, idx) => {
-	const subLinks = link.sub ? linksList(link.sub) : null,
-	      key = `link-${idx}`;
-
-	return (
-		<li key={ key }>
-			<a href={ link.url }>{ link.title }</a>
-			{ subLinks }
-		</li>
-	);
-}
-
-const linksList = links => (
-	<ul>
-		{ links.map(linksListItem) }
-	</ul>
+export default () => (
+	<main className="group">
+		{ MAIN_LINKS.map(pageLinksList) }
+	</main>
 )
 
 const pageLinksList = ({ title, links }, idx) => {
@@ -31,8 +19,20 @@ const pageLinksList = ({ title, links }, idx) => {
 	);
 }
 
-export default () => (
-	<main className="group">
-		{ MAIN_LINKS.map(pageLinksList) }
-	</main>
+const linksList = links => (
+	<ul>
+		{ links.map(linksListItem) }
+	</ul>
 )
+
+const linksListItem = (link, idx) => {
+	const subLinks = link.sub ? linksList(link.sub) : null,
+	      key = `link-${idx}`;
+
+	return (
+		<li key={ key }>
+			<a href={ link.url }>{ link.title }</a>
+			{ subLinks }
+		</li>
+	);
+}
