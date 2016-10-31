5b966adcd87890b9104b2aa9bbd8c1243b3863c6
Tyler Torola <tyler@tjt.codes>
2016-10-31 15:05:33 -0700

    moves insertDeskHash and adds :)

--- assets/src/entry.jsx
+++ assets/src/entry.jsx
@@ -4,13 +4,30 @@ import $ from 'jQuery';
 
 import Root from 'components/root';
 
+const smile = () => {
+  const $html = $("<b>:)</b>");
+
+  $html.css({
+    "display": "block",
+    "position": "fixed",
+    "font-size": "13px",
+    "bottom": "5px",
+    "right": "5px",
+    "-webkit-transform": "rotate(90deg)",
+    "font-family": "Inconsolata, monospace"
+  });
+
+  $("body").append($html);
+};
+
 const insertDeskHash = () => {
 	chrome.storage.local.get("deskHash", storage => {
 		$("input.desk-hash").val(storage.deskHash);
+		smile();
 	});
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	insertDeskHash();
 	ReactDom.render(<Root />, document.querySelector('#app'));
+	insertDeskHash();
 });
