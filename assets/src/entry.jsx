7f7a94646cae559dd3606fe478b3768fd23af710
Tyler Torola <tyler@tjt.codes>
2016-11-01 10:44:30 -0700

    fixes desk hash and adds input check

--- assets/src/entry.jsx
+++ assets/src/entry.jsx
@@ -5,29 +5,32 @@ import $ from 'jQuery';
 import Root from 'components/root';
 
 const smile = () => {
-  const $html = $("<b>:)</b>");
+	const $html = $("<b>:)</b>");
 
-  $html.css({
-    "display": "block",
-    "position": "fixed",
-    "font-size": "13px",
-    "bottom": "5px",
-    "right": "5px",
-    "-webkit-transform": "rotate(90deg)",
-    "font-family": "Inconsolata, monospace"
-  });
+	$html.css({
+		"display": "block",
+		"position": "fixed",
+		"bottom": "10px",
+		"right": "10px",
+		"-webkit-transform": "rotate(90deg)",
+		"font-family": "Inconsolata, monospace"
+	});
 
-  $("body").append($html);
+	$("body").append($html);
 };
 
 const insertDeskHash = () => {
 	chrome.storage.local.get("deskHash", storage => {
-		$("input.desk-hash").val(storage.deskHash);
-		smile();
+		const $input = $("input.desk-hash")
+		if ($input.length) {
+			$input.val(storage.deskHash);
+			smile();
+		}
 	});
 };
 
+insertDeskHash();
+
 document.addEventListener("DOMContentLoaded", () => {
 	ReactDom.render(<Root />, document.querySelector('#app'));
-	insertDeskHash();
 });
