edf448bbb225856a126bb51001f2ab8dbd17453f
Tyler Torola <tyler@tjt.codes>
2016-11-03 09:36:45 -0700

    untab webpack config as well

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,39 @@ const webpack = require('webpack');
 const PROD = JSON.parse(process.env.PROD_ENV || 'false');
 
 module.exports = {
-	context: __dirname,
-	entry: ["./assets/src/entry.jsx"],
-	output: {
-		path: path.resolve('./assets/build'),
-		filename: "bundle.js"
-	},
-	module: {
-		loaders: [
-			{
-				test: [/\.jsx?$/, /\.js?$/],
-				exclude: /(node_modules|bower_components)/,
-				loader: 'babel',
-				query: {
-					presets: ['es2015', 'react']
-				}
-			}
-		]
-	},
-	devtool: 'source-maps',
-	resolve: {
-		root: path.resolve('./assets/src'),
-		extensions: ["", ".js", ".jsx" ]
-	},
-	plugins: PROD ? [
-		new webpack.DefinePlugin({
-			'process.env':{
-				'NODE_ENV': JSON.stringify('production')
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress:{
-				warnings: false
-			}
-		})
-	] : []
+  context: __dirname,
+  entry: ["./assets/src/entry.jsx"],
+  output: {
+    path: path.resolve('./assets/build'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    root: path.resolve('./assets/src'),
+    extensions: ["", ".js", ".jsx" ]
+  },
+  plugins: PROD ? [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: false
+      }
+    })
+  ] : []
 };
