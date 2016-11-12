9f681ec4b20bb4502a33d368951d3fd9a231540e
Tyler Torola <tyler@tjt.codes>
2016-11-12 10:09:40 -0800

    tweaks play-pause

--- assets/build/bundle.js
+++ assets/build/bundle.js
@@ -36888,8 +36888,8 @@
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
-	var playPath = _react2.default.createElement("path", { className: "play", d: "M1576 927l-1328 738q-23 13-39.5 3t-16.5-36v-1472q0-26 16.5-36t39.5 3l1328 738q23 13 23 31t-23 31z" });
-	var pausePath = _react2.default.createElement("path", { className: "pause", d: "M1664 192v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45zm-896 0v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45z" });
+	var playPath = _react2.default.createElement("path", { d: "M1576 927l-1328 738q-23 13-39.5 3t-16.5-36v-1472q0-26 16.5-36t39.5 3l1328 738q23 13 23 31t-23 31z" });
+	var pausePath = _react2.default.createElement("path", { d: "M1664 192v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45zm-896 0v1408q0 26-19 45t-45 19h-512q-26 0-45-19t-19-45v-1408q0-26 19-45t45-19h512q26 0 45 19t19 45z" });
 	
 	exports.default = function (_ref) {
 		var playing = _ref.playing,
@@ -36897,7 +36897,7 @@
 		    pause = _ref.pause;
 		return _react2.default.createElement(
 			"svg",
-			{ width: "150", height: "150",
+			{ width: "120", height: "120",
 				className: "play-pause",
 				viewBox: "0 0 1792 1792",
 				xmlns: "http://www.w3.org/2000/svg",
