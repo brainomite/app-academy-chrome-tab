2a3b5519fc3b2e88f53e7a945f7eea30aa4ad121
Tyler Torola <tyler@tjt.codes>
2016-11-13 21:40:10 -0800

    adds basic gh oauth functions

--- assets/src/components/app/curriculum/view.jsx
+++ assets/src/components/app/curriculum/view.jsx
@@ -1,3 +1,18 @@
 import React from 'react';
 
-export default () => <div className="curriculum" />
+const handleResponse = setToken => redirectUrl => {
+  const githubToken = redirectUrl.split("code=")[1];
+}
+
+const getGithub = setToken => () => {
+  chrome.identity.launchWebAuthFlow({
+    url: "https://github.com/login/oauth/authorize?client_id=1a1e117ad08088ce339b",
+    interactive: true
+  }, handleResponse(setToken));
+}
+
+export default ({ setToken }) => (
+  <div className="curriculum">
+    <h1 onClick={ getGithub(setToken) }>Sign in to Github to view the daily curriculum.</h1>
+  </div>
+);
