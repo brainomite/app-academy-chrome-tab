94d970408118cea4e9a6c0730e308d62ecef78ea
Mashu Duek <mashuduek@gmail.com>
2018-08-10 11:22:35 -0400

    add logo and styling instead of rohmbus

--- assets/src/components/app/desk_info/view.jsx
+++ assets/src/components/app/desk_info/view.jsx
@@ -6,7 +6,10 @@ import Info from './info/container';
 export default ({ desk, showDesks }) => (
   <header className="desk-info wrap">
     <div className="clock-wrapper">
-      <img className="number" src="/assets/img/app-academy-logo-black.png"></img>
+      <img className="number"
+        onClick={ showDesks }
+        src="/assets/img/app-academy-logo-white.png">
+      </img>
       <Clock />
     </div>
     <Info />
