2bdd44327bc39a6b01b9dfb65579099754e34057
Oliver Ball <olivermartinball@gmail.com>
2018-08-15 16:12:32 -0400

    Make logo background rotate on hover

--- assets/src/components/app/desk_info/view.jsx
+++ assets/src/components/app/desk_info/view.jsx
@@ -6,10 +6,12 @@ import Info from './info/container';
 export default ({ desk, showDesks }) => (
   <header className="desk-info wrap">
     <div className="clock-wrapper">
-      <img className="number"
-        onClick={ showDesks }
-        src="/assets/img/app-academy-logo-white.png">
-      </img>
+      <div className="logo-box">
+        <img className="logo"
+          onClick={ showDesks }
+          src="/assets/img/app-academy-logo-white.png">
+        </img>
+      </div>
       <Clock />
     </div>
     <Info />
